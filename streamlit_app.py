e374849a8247de11b92fdb02e77ca9e8028b3ef9
20242808 <20242808@sdhs.gwe.hs.kr>
2024-12-15 19:49:18 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ if uploaded_file is not None:
     st.markdown("<h3>클래스별 확률:</h3>", unsafe_allow_html=True)
 
      if prediction == labels[0]:
-     st.write("T1")
+         st.write("T1")
      elif prediction == labels[1]:
          st.write("Gen.G")
      elif prediction == labels[2]:
